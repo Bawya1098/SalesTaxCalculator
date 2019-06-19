03e0fe7976002052e9970129d412e0969f0177b3
intern <bawya1098@gmail.com>
2019-06-19 22:29:19 +0530

    added Items and displayed Items name

---
+++ src/taxCalculatingSystem/BillingCounter.java
@@ -0,0 +1,30 @@
+package taxCalculatingSystem;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BillingCounter {
+    private Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        BillingCounter counter = new BillingCounter();
+        Bill bill = new Bill();
+        System.out.println("Enter Number of Items: ");
+        int noOfItems = counter.scan.nextInt();
+        counter.scan.nextLine();
+        List<Item> items = counter.addItem(bill, noOfItems);
+        bill.displayItemsName(items);
+    }
+
+    private List<Item> addItem(Bill bill, int noOfItems) {
+        for (int index = 0; index < noOfItems; index++) {
+            String[] strings = scan.nextLine().split(" ");
+            String itemName = strings[0];
+            int quantity = Integer.parseInt(strings[1]);
+            double price = Double.parseDouble(strings[2]);
+            Item item = new Item(itemName, quantity, price);
+            bill.items.add(item);
+        }
+        return bill.items;
+    }
+}
