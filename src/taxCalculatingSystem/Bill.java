316101f09edf25c817488ec8aa165739cbbd6476
intern <bawya1098@gmail.com>
2019-06-20 11:47:41 +0530

    Moved add Item method to Bill

--- src/taxCalculatingSystem/Bill.java
+++ src/taxCalculatingSystem/Bill.java
@@ -2,6 +2,7 @@ package taxCalculatingSystem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Bill {
     List<Item> items = new ArrayList<>();
@@ -10,5 +11,17 @@ public class Bill {
         for (Item item : items) {
             System.out.println(item.toString());
         }
+    }
+
+    public List<Item> addItem(Scanner scan, int noOfItems) {
+        for (int index = 0; index < noOfItems; index++) {
+            String[] strings = scan.nextLine().split(" ");
+            int quantity = Integer.parseInt(strings[0]);
+            String itemName = strings[1];
+            double price = Double.parseDouble(strings[3]);
+            Item item = new Item(itemName, quantity, price);
+            items.add(item);
+        }
+        return items;
     }
 }
--- src/taxCalculatingSystem/BillingCounter.java
+++ src/taxCalculatingSystem/BillingCounter.java
@@ -4,16 +4,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BillingCounter {
-    private Scanner scan = new Scanner(System.in);
     private static final double tax = 0.15;
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         BillingCounter counter = new BillingCounter();
         Bill bill = new Bill();
         System.out.println("Enter Number of Items: ");
-        int noOfItems = counter.scan.nextInt();
-        counter.scan.nextLine();
-        List<Item> items = counter.addItem(bill, noOfItems);
+        int noOfItems = scan.nextInt();
+        scan.nextLine();
+        List<Item> items = bill.addItem(scan, noOfItems);
         counter.calculateTax(items);
         bill.displayItemsName(items);
     }
@@ -26,15 +26,4 @@ public class BillingCounter {
         }
     }
 
-    private List<Item> addItem(Bill bill, int noOfItems) {
-        for (int index = 0; index < noOfItems; index++) {
-            String[] strings = scan.nextLine().split(" ");
-            int quantity = Integer.parseInt(strings[0]);
-            String itemName = strings[1];
-            double price = Double.parseDouble(strings[3]);
-            Item item = new Item(itemName, quantity, price);
-            bill.items.add(item);
-        }
-        return bill.items;
-    }
 }
