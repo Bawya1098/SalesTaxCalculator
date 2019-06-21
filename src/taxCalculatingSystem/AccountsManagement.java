0073f2e241f81344d402b5d503cd16159d21aa99
intern <bawya1098@gmail.com>
2019-06-21 11:24:20 +0530

    Moved add Item method to Items

--- src/taxCalculatingSystem/AccountsManagement.java
+++ src/taxCalculatingSystem/AccountsManagement.java
@@ -1,41 +1,22 @@
 package taxCalculatingSystem;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class AccountsManagement {
-    private List<Item> items = new ArrayList<>();
-    private static final double totalTax = 0.15;
+    private List<String> exemptedItems = Arrays.asList("book", "Doll", "medicine", "chocolate");
+    private static double tax = 0.15;
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         AccountsManagement management = new AccountsManagement();
+        Scanner scan = new Scanner(System.in);
+        Items items = new Items();
         System.out.println("Enter Number of Items: ");
-        int noOfItems = scan.nextInt();
-        scan.nextLine();
-        List<Item> items = management.addItem(scan, noOfItems);
-        management.displayItemsName(items);
+        int noOfItems = Integer.parseInt(scan.nextLine());
+        items.addItem(scan, noOfItems);
+        items.calculateTax(management.exemptedItems, tax);
+        items.displayItemsName();
     }
-
-    private List<Item> addItem(Scanner scan, int noOfItems) {
-        for (int index = 0; index < noOfItems; index++) {
-            String[] strings = scan.nextLine().split(" ");
-            int quantity = Integer.parseInt(strings[0]);
-            String itemName = strings[1];
-            double price = Double.parseDouble(strings[3]);
-            Item item = new Item(itemName, quantity, price,AccountsManagement.totalTax);
-            items.add(item);
-        }
-        return items;
-    }
-
-    private void displayItemsName(List<Item> items) {
-        for (Item item : items) {
-            System.out.println(item.toString());
-        }
-    }
-
-
-
 }
+
